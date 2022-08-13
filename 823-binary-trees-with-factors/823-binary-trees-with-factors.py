012f63a7d1805fb22b0752f148d61f2f7c29d6c8
nitin22032002 <79556510+nitin22032002@users.noreply.github.com>
2022-08-13 17:00:15 +0530

    Time: 1993 ms (11.61%), Space: 14.2 MB (30.81%) - LeetHub

--- 823-binary-trees-with-factors/823-binary-trees-with-factors.py
+++ 823-binary-trees-with-factors/823-binary-trees-with-factors.py
@@ -4,22 +4,22 @@ class Solution:
         arr.sort()
         d={}
         for item in arr:
-            ans+=self.get(set(arr),item,d)
+            ans+=self.get(arr,item,d,set(arr))
         # print(d)
         return (ans)%((10**9)+7)
     
-    def get(self,nums,target,d):
+    def get(self,nums,target,d,arr):
         if(target==1):
             return 1
         elif((target) not in d):
             ans=0
-            for item in nums:
-                if(target%item==0 and (target//item) in nums):
-                    ans+=(self.get(nums,target//item,d)*self.get(nums,item,d))
+            i=0
+            while(i<len(nums) and nums[i]<=target):
+                item=nums[i]
+                if(target%item==0 and (target//item) in arr):
+                    ans+=(self.get(nums,target//item,d,arr)*self.get(nums,item,d,arr))
                 elif(target==item):
                     ans+=1
-                # else:
-                #     print(target,item)
-                
+                i+=1
             d[target]=(ans)%((10**9)+7)
         return d[target]

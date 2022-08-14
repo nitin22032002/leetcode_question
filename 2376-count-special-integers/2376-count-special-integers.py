e82cabdf21981ea17f8c54425145b766dd874915
nitin22032002 <79556510+nitin22032002@users.noreply.github.com>
2022-08-14 23:06:16 +0530

    Time: 721 ms (30.00%), Space: 14.5 MB (30.00%) - LeetHub

---
+++ 2376-count-special-integers/2376-count-special-integers.py
@@ -0,0 +1,46 @@
+class Solution:
+    def countSpecialNumbers(self, n: int) -> int:
+        nums=[]
+        while(n!=0):
+            nums.append(n%10)
+            n//=10
+        nums.reverse()
+        ans=0
+        d={}
+        for i in range(nums[0]+1):
+            if(i==0):
+                bit=0
+            else:
+                bit=1<<i
+            if(i==nums[0] and len(nums)>1):
+                ans+=self.get(nums,1,nums[1],bit,d)
+            else:
+                ans+=self.get(nums,1,10,bit,d)
+        return ans-1
+    
+    def get(self,nums,i,end,bits,d):
+        if(i>=len(nums)):
+            # self.findBits(bits)
+            return 1
+        elif((i,bits,end) in d):
+            return d[(i,bits,end)]
+        else:
+            ans=0
+            if(end==10):
+                last=10
+            else:
+                last=end+1
+            for j in range(last):
+                b=1<<j
+                if(j==0 and bits==0):
+                    if((nums[i]==end and end==j) and i!=len(nums)-1):
+                        ans+=self.get(nums,i+1,nums[i+1],bits,d)
+                    else:
+                        ans+=self.get(nums,i+1,10,bits,d)
+                elif(bits&b==0):
+                    if((end==j) and i!=len(nums)-1):
+                        ans+=self.get(nums,i+1,nums[i+1],bits|b,d)
+                    else:
+                        ans+=self.get(nums,i+1,10,bits|b,d)
+            d[(i,bits,end)]=ans
+            return ans

6232005dc214cdca2a3b7c4da1284614b24e1067
nitin22032002 <79556510+nitin22032002@users.noreply.github.com>
2022-09-14 18:45:58 +0530

    Time: 87 ms (23.81%), Space: 16.4 MB (80.83%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.py
@@ -6,23 +6,11 @@
 #         self.right = right
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        ans=[True]
-        self.INT=-pow(2,31)-1
-        self.get(root,ans)
-        return ans[0]
-    def get(self,root,g):
-        if(root and g[0]):
-            a=self.get(root.left,g)
-            if(a[0]!=self.INT):
-                if(root.val<=a[1]):
-                    g[0]=False
-            b=self.get(root.right,g)
-            if(b[0]!=self.INT):
-                if(root.val>=b[0]):
-                    g[0]=False
-            if(a[0]==self.INT):
-                a[0]=root.val
-            if(b[1]==self.INT):
-                b[1]=root.val
-            return [a[0],b[1]]
-        return [self.INT,self.INT]
+        return self.get(root,-(2**31)-1,(2**31))
+    def get(self,root,min_val,max_val):
+        if(root):
+            if(root.val<=min_val or root.val>=max_val):
+                return False
+            return self.get(root.left,min_val,root.val) and self.get(root.right,root.val,max_val)
+        return True
+        

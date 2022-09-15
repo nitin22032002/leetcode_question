76578fff2d9b174b6434bf82fc69df332174e52e
nitin22032002 <79556510+nitin22032002@users.noreply.github.com>
2022-09-15 08:20:41 +0530

    Time: 58 ms (22.59%), Space: 13.8 MB (67.07%) - LeetHub

--- 779-k-th-symbol-in-grammar/779-k-th-symbol-in-grammar.py
+++ 779-k-th-symbol-in-grammar/779-k-th-symbol-in-grammar.py
@@ -2,14 +2,12 @@ class Solution:
     def kthGrammar(self, n: int, k: int) -> int:
         start=1
         end=pow(2,n-1)
-        return self.get(start,end,False,k)
+        return self.get(start,end,0,k)
     def get(self,start,end,target,k):
         mid=(start+end)//2
         if(start==end):
-            if(target):
-                return 1
-            return 0
+            return target
         if(k<=mid):
             return self.get(start,mid,target,k)
         else:
-            return self.get(mid+1,end,not target,k)
+            return self.get(mid+1,end,target^1,k)

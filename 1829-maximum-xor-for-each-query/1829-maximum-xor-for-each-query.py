ae6dd0c39edfc0f118015f5c0a65dd3c54436b9d
nitin22032002 <79556510+nitin22032002@users.noreply.github.com>
2022-09-15 07:10:46 +0530

    Time: 4975 ms (6.10%), Space: 33.4 MB (27.70%) - LeetHub

--- 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.py
+++ 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.py
@@ -1,32 +1,17 @@
 class Solution:
     def getMaximumXor(self, nums: List[int], maximumBit: int) -> List[int]:
-        m=nums[-1]
-        bits=0
-        while(m!=0):
-            bits+=1
-            m//=2
-        arr=[0 for _ in range(max(m,maximumBit))]
-        for item in nums:
-            for i in range(len(arr)):
-                b=1<<i
-                if(b&item!=0):
-                    arr[i]+=1
-        ans=[0 for _ in range(len(nums))]
-        i=0
+        xor=nums[0]
+        for i in range(1,len(nums)):
+            xor^=nums[i]
         j=len(nums)-1
+        ans=[]
         while(j>=0):
-            # print(arr)
-            res=0
-            for k in range(maximumBit):
-                if(arr[k]%2==0):
-                    b=1<<k
-                    res|=b
-            ans[i]=res
-            for k in range(len(arr)):
-                b=1<<k
-                if(b&nums[j]!=0):
-                    arr[k]-=1
+            k=0
+            for i in range(maximumBit):
+                b=1<<i
+                if(xor&b==0):
+                    k|=b
+            ans.append(k)
+            xor^=nums[j]
             j-=1
-            i+=1
         return ans
-        

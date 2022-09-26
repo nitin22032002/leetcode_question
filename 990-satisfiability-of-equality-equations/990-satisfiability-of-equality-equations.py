322f0118d3ba52f2674952d8e16df5781506da23
nitin22032002 <79556510+nitin22032002@users.noreply.github.com>
2022-09-27 00:01:55 +0530

    Time: 119 ms (5.34%), Space: 14.2 MB (9.51%) - LeetHub

--- 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.py
+++ 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.py
@@ -1,40 +1,24 @@
 class Solution:
     def equationsPossible(self, equations: List[str]) -> bool:
-        state=[[0 for _ in range(26)] for __ in range(26)]
-        for i in range(26):
-            state[i][i]=1
         parent=[i for i in range(26)]
         rank=[0 for _ in range(26)]
         for item in equations:
             a,b,c,d=list(item)
             a=ord(a)-97
             d=ord(d)-97
-            if(b=="!"):  
-                val=2
-            else:
-                val=1
             p1=self.findParent(parent,a)
             p2=self.findParent(parent,d)
-            # print(chr(p1+97),chr(p2+97),state[p1][p2],val)
-            if((state[p1][p2]==0 or state[p1][p2]==val) and (state[p2][p1]==0 or state[p2][p1]==val)):
-                if(val==1):
-                    if(rank[p1]>rank[p2]):
-                        parent[p2]=p1
-                    elif(rank[p1]==rank[p2]):
-                        parent[p1]=p2
-                        rank[p2]+=1
-                    else:
-                        parent[p1]=p2
-                    for i in range(26):
-                        if(state[p2][i]!=0):
-                            state[p1][i]=state[p2][i]
-                        if(state[p1][i]!=0):
-                            state[p2][i]=state[p1][i]
-                state[p1][p2]=val
-                state[p2][p1]=val
-            elif(state[p1][p2]!=val):
-                # print(chr(p1+97),chr(p2+97),state[p1][p2],val,state[p2][p1])
+            if(p1!=p2 and b=="="):
+                self.union(parent,p1,p2,rank)
+        for item in equations:
+            a,b,c,d=list(item)
+            a=ord(a)-97
+            d=ord(d)-97
+            p1=self.findParent(parent,a)
+            p2=self.findParent(parent,d)
+            if(p1==p2 and b=="!"):
                 return False
+                
         return True
     
     def findParent(self,parent,node):
@@ -42,3 +26,11 @@ class Solution:
             return node
         parent[node]=self.findParent(parent,parent[node])
         return parent[node]
+    def union(self,parent,node1,node2,rank):
+        if(rank[node1]==rank[node2]):
+            parent[node1]=node2
+            rank[node2]+=1
+        elif(rank[node1]>rank[node2]):
+            parent[node2]=node1
+        else:
+            parent[node1]=node2

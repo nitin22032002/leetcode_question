8cc1e12820c362e68f7e3554710f49adac33e30d
nitin22032002 <79556510+nitin22032002@users.noreply.github.com>
2022-10-01 18:14:03 +0530

    Time: 384 ms (83.45%), Space: 14 MB (77.24%) - LeetHub

--- 1577-number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers/1577-number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers.py
+++ 1577-number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers/1577-number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers.py
@@ -3,38 +3,22 @@ class Solution:
         d1={}
         d2={}
         for i in range(len(nums1)):
-            if(nums1[i] in d1):
-                d1[nums1[i]].append(i)
+            if((nums1[i]*nums1[i]) in d1):
+                d1[nums1[i]*nums1[i]]+=1
             else:
-                d1[nums1[i]]=[i]
+                d1[nums1[i]*nums1[i]]=1
         for i in range(len(nums2)):
-            if(nums2[i] in d2):
-                d2[nums2[i]].append(i)
+            if((nums2[i]*nums2[i]) in d2):
+                d2[nums2[i]*nums2[i]]+=1
             else:
-                d2[nums2[i]]=[i]
-        ans=self.solve(nums1,nums2,d2)+self.solve(nums2,nums1,d1)
+                d2[nums2[i]*nums2[i]]=1
+        ans=self.solve(nums1,d2)+self.solve(nums2,d1)
         return ans
-    def solve(self,nums1,nums2,d):
+    def solve(self,nums,d):
         ans=0
-        for item in nums1:
-            target=item*item
-            for i in range(len(nums2)):
-                if(target%nums2[i]==0 and (target//nums2[i]) in d):
-                    arr=d[target//nums2[i]]
-                    ans+=(len(arr)-self.upperBound(arr,i+1))
+        for i in  range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if(nums[i]*nums[j] in d):
+                    ans+=d[nums[i]*nums[j]]
         return ans
-    def upperBound(self,arr,target):
-        end=len(arr)-1
-        start=0
-        while(start<end-1):
-            mid=(start+end)//2
-            if(arr[mid]<target):
-                start=mid
-            else:
-                end=mid
-        if(arr[start]>=target):
-            return start
-        elif(arr[end]>=target):
-            return end
-        return len(arr)
         

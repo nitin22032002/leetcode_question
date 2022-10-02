398606dd8cc41ba9f37d4dbeaaf5ae3606f39914
nitin22032002 <79556510+nitin22032002@users.noreply.github.com>
2022-10-02 22:40:31 +0530

    Time: 1606 ms (65.07%), Space: 16.6 MB (35.04%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.py
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.py
@@ -1,14 +1,15 @@
 class Solution:
     def minInsertions(self, s: str) -> int:
-        return self.get(s,0,len(s)-1,{})
+        dp=[[-1 for _ in range(len(s))] for __ in range(len(s))]
+        return self.get(s,0,len(s)-1,dp)
     def get(self,s,i,j,dp):
         if(i>=j):
             return 0
-        elif((i,j) in dp):
-            return dp[(i,j)]
+        elif(dp[i][j]!=-1):
+            return dp[i][j]
         else:
             if(s[i]==s[j]):
-                dp[(i,j)]=self.get(s,i+1,j-1,dp)
+                dp[i][j]=self.get(s,i+1,j-1,dp)
             else:
-                dp[(i,j)]=min(self.get(s,i+1,j,dp),self.get(s,i,j-1,dp))+1
-            return dp[(i,j)]
+                dp[i][j]=min(self.get(s,i+1,j,dp),self.get(s,i,j-1,dp))+1
+            return dp[i][j]

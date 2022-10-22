175ee0b04ce8cfbc33723085abae879a35dcdc7f
nitin22032002 <79556510+nitin22032002@users.noreply.github.com>
2022-10-22 09:27:48 +0530

    Time: 1414 ms (67.14%), Space: 21.3 MB (73.01%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -1,14 +1,13 @@
 class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
-        dp=[[-1,-1] for _ in range(len(prices))]
-        return self.get(prices,0,0,fee,dp)
-    def get(self,price,i,state,fee,dp):
-        if(i>=len(price)):
+        if(len(prices)<2):
             return 0
-        elif(dp[i][state]==-1):
-            if(state==0):
-                ans=max(self.get(price,i+1,state,fee,dp),self.get(price,i+1,1,fee,dp)-price[i]-fee)
-            else:
-                ans=max(self.get(price,i+1,state,fee,dp),self.get(price,i+1,0,fee,dp)+price[i])
-            dp[i][state]=ans
-        return dp[i][state]
+        start=prices[0]
+        cost=0
+        for i in range(1,len(prices)):
+            if(start>prices[i]):
+                start=prices[i]
+            elif(prices[i]>=(fee+start)):
+                cost+=(prices[i]-fee-start)
+                start=prices[i]-fee
+        return cost

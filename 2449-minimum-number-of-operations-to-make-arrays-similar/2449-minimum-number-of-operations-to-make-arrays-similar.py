a39b48400905b99f8962eeb2d797a7f2a78d421f
nitin22032002 <79556510+nitin22032002@users.noreply.github.com>
2022-10-24 08:25:51 +0530

    Time: 1435 ms (75.00%), Space: 34.2 MB (8.33%) - LeetHub

--- 2449-minimum-number-of-operations-to-make-arrays-similar/2449-minimum-number-of-operations-to-make-arrays-similar.py
+++ 2449-minimum-number-of-operations-to-make-arrays-similar/2449-minimum-number-of-operations-to-make-arrays-similar.py
@@ -1,44 +1,9 @@
 class Solution:
     def makeSimilar(self, nums: List[int], target: List[int]) -> int:
-        nums.sort()
-        target.sort()
-        odd=[]
-        even=[]
-        opt=[]
-        i=0
-        j=0
-        while(i<len(nums) and j<len(target)):
-            if(nums[i]==target[j]):
-                i+=1
-                j+=1
-            elif(nums[i]>target[j]):
-                opt.append(target[j])
-                j+=1
-            else:
-                if(nums[i]%2==0):
-                    even.append(nums[i])
-                else:
-                    odd.append(nums[i])
-                i+=1
-        while(i<len(nums)):
-            if(nums[i]%2==0):
-                    even.append(nums[i])
-            else:
-                    odd.append(nums[i])
-            i+=1
-        s1=0
-        s2=0
-        while(j<len(target)):
-            opt.append(target[j])
-            j+=1
+        nums.sort(key=lambda x:[x%2,x])
+        target.sort(key=lambda x:[x%2,x])
         ans=0
-        # print(opt,even,odd)
-        for item in opt:
-            if(item%2==0):
-                cost=abs(item-even[s1])//2
-                s1+=1
-            else:
-                cost=abs(item-odd[s2])//2
-                s2+=1
-            ans+=cost
+        for i in range(len(target)):
+            ans+=abs(nums[i]-target[i])//2
         return ans//2
+        

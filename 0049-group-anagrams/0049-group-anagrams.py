a0c3d831b13196c50f3f0bb118cc446a60311119
nitin22032002 <79556510+nitin22032002@users.noreply.github.com>
2022-10-28 08:15:12 +0530

    Time: 472 ms (5.01%), Space: 17.2 MB (88.69%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -2,9 +2,18 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         d={}
         for item in strs:
-            item1="".join(sorted(item))
+            item1=self.sorted(item)
             if(item1 in d):
                 d[item1].append(item)
             else:
                 d[item1]=[item]
         return list(d.values())
+    def sorted(self,s):
+        d=[0 for _ in range(26)]
+        for item in s:
+            d[ord(item)-97]+=1
+        ans=[]
+        for i in range(26):
+            if(d[i]!=0):
+                ans.append(chr(i+97)*d[i])
+        return "".join(ans)

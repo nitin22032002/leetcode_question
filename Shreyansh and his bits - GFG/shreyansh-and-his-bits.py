ff2f794efc12a6e2d662e97a06255d1c07fcefeb
nitin22032002 <79556510+nitin22032002@users.noreply.github.com>
2022-11-25 18:29:04 +0530

    Added solution - LeetHub

--- Shreyansh and his bits - GFG/shreyansh-and-his-bits.py
+++ Shreyansh and his bits - GFG/shreyansh-and-his-bits.py
@@ -1,30 +1,24 @@
 class Solution:
     def count (self, N):
         arr=[]
+        c=0
         while(N!=0):
             arr.append(N%2)
+            c+=(N%2)
             N//=2
         arr.reverse()
         # print(arr)
-        ans=self.get(arr,0)
+        ans=self.get(arr,0,c)
         return ans-1
-    def get(self,arr,i):
-        # print("i",i)
+    def get(self,arr,i,c):
         if(i>=len(arr)):
             return 1-arr[i-1]
         elif(arr[i]==0):
-            return self.get(arr,i+1)
+            return self.get(arr,i+1,c)
         else:
-            c=self.count_bit(arr,i)
             ans=(self.factorial(len(arr)-i-1)//(self.factorial(c)*self.factorial(len(arr)-i-c-1)))
-            ans+=self.get(arr,i+1)
+            ans+=self.get(arr,i+1,c-1)
             return ans
-    def count_bit(self,arr,i):
-        ans=0
-        for i in range(i,len(arr)):
-            if(arr[i]==1):
-                ans+=1
-        return ans
     def factorial(self,n):
         # print(n)
         ans=1

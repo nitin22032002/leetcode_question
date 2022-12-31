6d49c6d87c622b19d674b5cd6ec30ce2a0876f67
nitin22032002 <79556510+nitin22032002@users.noreply.github.com>
2022-12-31 08:53:29 +0530

    Added solution - LeetHub

---
+++ Find minimum number of Laptops required - GFG/find-minimum-number-of-laptops-required.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    int minLaptops(int N, int start[], int end[]) {
+        vector<pair<int,int>> arr;
+        for(int i=0;i<N;i++){
+            arr.push_back({start[i],end[i]});
+        }
+        sort(arr.begin(),arr.end());
+        priority_queue<int,vector<int>,greater<int>> d;
+        int ans=0;
+        for(int i=0;i<N;i++){
+            if(d.size()==0 or d.top()>arr[i].first){
+                d.push(arr[i].second);
+                ans++;
+            }
+            else{
+            d.pop();
+            d.push(arr[i].second);
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        int start[N], end[N];
+        for(int i=0; i<N; i++)
+            cin>>start[i];
+        for(int i=0; i<N; i++)
+            cin>>end[i];
+            
+        Solution ob;
+        cout << ob.minLaptops(N, start, end) << endl;
+    }
+}
+// } Driver Code Ends

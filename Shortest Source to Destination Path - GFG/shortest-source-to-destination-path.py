45e238338f2c864f67ff2d184c46c91c39df681c
nitin22032002 <79556510+nitin22032002@users.noreply.github.com>
2023-01-02 11:54:38 +0530

    Added solution - LeetHub

---
+++ Shortest Source to Destination Path - GFG/shortest-source-to-destination-path.py
@@ -0,0 +1,42 @@
+#User function Template for python3
+from queue import Queue
+class Solution:
+    def shortestDistance(self,N,M,A,X,Y):
+        obj=Queue()
+	    visited=[[-1 for _ in range(M)] for __ in range(N)]
+	    obj.put([0,0,0])
+	    visited[0][0]=0
+	    if(A[0][0]==0 or A[X][Y]==0):return -1
+	    while(not obj.empty()):
+	        cost,i,j=obj.get()
+	        if(i==X and j==Y):
+	            return cost
+	        else:
+	            for i1,j1 in [[i,j-1],[i,j+1],[i-1,j],[i+1,j]]:
+	                if(i1>=0 and i1<N and j1>=0 and j1<M and A[i1][j1]==1):
+	                    if(visited[i1][j1]==-1 or visited[i1][j1]>1+cost):
+	                        visited[i1][j1]=1+cost
+	                        obj.put([visited[i1][j1],i1,j1])
+	        
+	    return -1
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import math
+        
+if __name__=='__main__':
+    t=int(input())
+    for _ in range(t):
+        N,M=map(int,input().strip().split())
+        a=[]
+        for i in range(N):
+            s=list(map(int,input().strip().split()))
+            a.append(s)
+        x,y=map(int,input().strip().split())    
+        ob=Solution()
+        print(ob.shortestDistance(N,M,a,x,y))
+        
+# } Driver Code Ends

9eebdda4adcd51011989dc9150fb3288b9ca42e2
nitin22032002 <79556510+nitin22032002@users.noreply.github.com>
2023-02-07 18:33:24 +0530

    Added solution - LeetHub

---
+++ Length of the longest subarray with positive product - GFG/length-of-the-longest-subarray-with-positive-product.py
@@ -0,0 +1,54 @@
+#User function Template for python3
+
+
+class Solution:
+    def maxLength(self,arr,n):
+        l=r=-1
+        ans=i=j=p=0
+        while(j<n):
+            if(arr[j]==0):
+                # print((i,j,l,r))
+                if(p%2==0):
+                    ans=max(ans,j-i)
+                else:
+                    ans=max(ans,j-l-1,r-i)
+                l=r=-1
+                p=0
+                i=j+1
+            else:
+                if(arr[j]<0):
+                    p+=1
+                    if(l==-1):
+                        l=j
+                    r=j
+            j+=1
+        if(p%2==0):
+            ans=max(ans,j-i)
+        else:
+            ans=max(ans,j-l-1,r-i)
+        return ans
+                
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+def main():
+        T=int(input())
+        while(T>0):
+            
+            n=int(input())
+
+            arr=[int(x) for x in input().strip().split()]
+            
+            
+            obj = Solution()
+            print(obj.maxLength(arr,n))
+            
+            T-=1
+
+
+if __name__ == "__main__":
+    main()
+# } Driver Code Ends

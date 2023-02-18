d07d48fb8d463289f6505403ce666e96aca87213
nitin22032002 <79556510+nitin22032002@users.noreply.github.com>
2023-02-18 08:56:05 +0530

    Added solution - LeetHub

---
+++ Apple Sequences - GFG/apple-sequences.py
@@ -0,0 +1,44 @@
+#User function Template for python3
+
+class Solution:
+    def appleSequences(self, n, m, arr):
+        i=0
+        j=0
+        c=0
+        ans=0
+        while(j<n):
+            if(c<=m):
+                ans=max(ans,j-i)
+                if(arr[j]=='O'):
+                    c+=1
+                j+=1
+            else:
+                if(arr[i]=='O'):
+                    c-=1
+                i+=1
+        while(i<n and c>m):
+            if(arr[i]=='O'):
+                c-=1
+            i+=1
+        ans=max(ans,j-i)
+        return ans
+        
+        
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__': 
+    t = int (input ())
+    for _ in range (t):
+        temp = input().split()
+        N = int(temp[0])
+        M = int(temp[1])
+        arr = input()
+
+        ob = Solution()
+        print(ob.appleSequences(N, M, arr))
+
+# } Driver Code Ends

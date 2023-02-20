714f78bb00f11edec2aa0b753d47e7865fb57b22
nitin22032002 <79556510+nitin22032002@users.noreply.github.com>
2023-02-20 16:28:48 +0530

    Added solution - LeetHub

--- Count Cyclic Paths - GFG/count-cyclic-paths.py
+++ Count Cyclic Paths - GFG/count-cyclic-paths.py
@@ -1,6 +1,5 @@
 #User function Template for python3
-import sys
-sys.setrecursionlimit(10**7)
+
 class Solution:
     def countPaths (self, N):
         mod=int(1e9+7)
@@ -8,7 +7,7 @@ class Solution:
         for i in range(1,N+1):
             a,b=(3*b)%mod,((2*b)%mod+(a)%mod)%mod
         return a
-                
+        
 
 
 #{ 

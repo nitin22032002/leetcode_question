0e83538d2513718f03cf2737a1213004f917a0ed
nitin22032002 <79556510+nitin22032002@users.noreply.github.com>
2023-04-11 10:05:40 +0530

    Added solution - LeetHub

---
+++ Maximum Length - GFG/maximum-length.py
@@ -0,0 +1,25 @@
+#User function Template for python3
+
+class Solution():
+    def solve(self, a, b, c):
+        r=a+b+c
+        a1,b1,c1=sorted([a,b,c],reverse=True)
+        t1=(2*c1)+1
+        t2=(b1-c1)
+        if(a1>=(t1+t2)*2):return -1
+        return r
+        
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+from collections import Counter
+for _ in range(int(input())):
+    a, b, c=map(int,input().split())
+    obj = Solution()
+    res = obj.solve(a, b, c)
+    
+    print(res)
+# } Driver Code Ends

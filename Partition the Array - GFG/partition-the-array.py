f28f60796fad73eef2bc57957d8d660197ac2fab
nitin22032002 <79556510+nitin22032002@users.noreply.github.com>
2023-04-13 21:34:38 +0530

    Added solution - LeetHub

---
+++ Partition the Array - GFG/partition-the-array.py
@@ -0,0 +1,56 @@
+#User function Template for python3
+class Solution:
+    
+    def minDifference(self, N, A): 
+        #code here
+        x = self.partition(A,N)
+        A.reverse()
+        y=  self.partition(A,N)
+        y.reverse()
+        ans = 10**18
+        for i in range(1,N-2):
+            ans=min(ans,max(x[i][0],x[i][1], y[i+1][0],y[i+1][1]) - min(x[i][0],x[i][1], y[i+1][0],y[i+1][1]))
+        return ans
+            
+    def partition(self,A,n):
+        tmp = [(0,0)]
+        p = A[:]
+        for i in range(1,n):
+            p[i] += p[i-1]
+        for i in range(1,n):
+            diff = 10**18
+            hi = i
+            lo = 1
+            ans = (0,0)
+            while lo <= hi:
+                mid = (lo + hi) // 2
+                x = p[mid-1]
+                y = p[i] - x
+                if abs(x-y) <diff:
+                    diff = abs(x-y)
+                    ans = (x,y)
+                if x > y:
+                    hi = mid-1
+                else:
+                    lo = mid + 1
+            tmp.append(ans)
+        return tmp
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+
+if __name__ == '__main__': 
+    t = int(input())
+    for _ in range(t):
+        N = int(input())
+        A = list(map(int, input().strip().split()))
+        ob = Solution()
+        ans = ob.minDifference(N, A)
+        print(ans)
+
+
+# } Driver Code Ends

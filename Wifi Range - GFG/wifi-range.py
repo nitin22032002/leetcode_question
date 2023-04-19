8eea1c4f205422a8a52de6380febd8bfe1e0192a
nitin22032002 <79556510+nitin22032002@users.noreply.github.com>
2023-04-19 19:46:24 +0530

    Added solution - LeetHub

---
+++ Wifi Range - GFG/wifi-range.py
@@ -0,0 +1,42 @@
+#User function Template for python3
+class Solution:
+    def wifiRange(self, N, S, X,rever=True): 
+        count=0
+        for i in range(min(X+1,N)):
+            if(S[i]=='1'):count+=1
+        i=-X
+        j=X+1
+        k=0
+        # print(i,j)
+        while(k<N):
+            if(S[k]=="0" and count==0):
+                # print(k,i,j)
+                return False
+            if(i>=0 and S[i]=='1'):
+                count-=1
+            if(j<N and S[j]=='1'):
+                count+=1
+            i+=1
+            j+=1
+            k+=1
+        return True
+            
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__': 
+    t = int(input())
+    for _ in range(t):
+        N,X = map(int,input().strip().split())
+        S = input()
+        ob = Solution()
+        ans = ob.wifiRange(N, S, X)
+        if ans:
+            print(1)
+        else:
+            print(0)
+
+# } Driver Code Ends

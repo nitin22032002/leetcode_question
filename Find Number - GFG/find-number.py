69cbf11940288aef978464d7a86f8caac873ea3d
nitin22032002 <79556510+nitin22032002@users.noreply.github.com>
2023-04-29 17:39:02 +0530

    Added solution - LeetHub

---
+++ Find Number - GFG/find-number.py
@@ -0,0 +1,39 @@
+#User function Template for python3
+
+class Solution:
+    def findNumber(self, N : int) -> int:
+        val=[1,3,5,7,9]
+        ans=[]
+        mul=5
+        pre=1
+        while(N>0):
+            r=(N%mul)-1
+            r=(mul+r)%mul
+            r//=pre
+            ans.append(str(val[r]))
+            N-=mul
+            mul*=5
+            pre*=5
+        ans.reverse()
+        ans="".join(ans)
+        return int(ans)
+            
+        
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__=="__main__":
+    t = int(input())
+    for _ in range(t):
+        
+        N = int(input())
+        
+        obj = Solution()
+        res = obj.findNumber(N)
+        
+        print(res)
+        
+# } Driver Code Ends

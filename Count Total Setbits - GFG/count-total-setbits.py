094fc9a96be96c14e1ce63b4cf15a84045c6510e
nitin22032002 <79556510+nitin22032002@users.noreply.github.com>
2023-05-15 17:52:25 +0530

    Added solution - LeetHub

---
+++ Count Total Setbits - GFG/count-total-setbits.py
@@ -0,0 +1,31 @@
+
+
+class Solution:
+    def countBits(self, N : int) -> int:
+        ans=0
+        for i in range(32):
+            x=1<<i
+            ans+=((N+1)//(2*x))*x
+            r=((N+1)%(x*2))-x
+            ans+=max(r,0)
+        return ans
+        
+        
+
+
+
+#{ 
+ # Driver Code Starts
+if __name__=="__main__":
+    t = int(input())
+    for _ in range(t):
+        
+        N = int(input())
+        
+        obj = Solution()
+        res = obj.countBits(N)
+        
+        print(res)
+        
+
+# } Driver Code Ends

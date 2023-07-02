54898cfcda8ff88cf0d7af747587c6724f37e901
nitin22032002 <79556510+nitin22032002@users.noreply.github.com>
2023-07-02 10:32:14 +0530

    Added solution - LeetHub

---
+++ Copy Set Bits in Range - GFG/copy-set-bits-in-range.py
@@ -0,0 +1,25 @@
+#User function Template for python3
+
+class Solution:
+    def setSetBit(self, x, y, l, r):
+        for i in range(l-1,r):
+            if((y>>i)&1==1):
+                x|=(1<<i)
+        return x
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        arr = input().split()
+        x = int(arr[0])
+        y = int(arr[1])
+        l = int(arr[2])
+        r = int(arr[3])
+        
+        ob = Solution()
+        print(ob.setSetBit(x, y, l, r))
+# } Driver Code Ends

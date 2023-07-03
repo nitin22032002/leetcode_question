5068325448e39b6fa1c4de2f04c91cb6c4c4fb65
nitin22032002 <79556510+nitin22032002@users.noreply.github.com>
2023-07-03 09:07:42 +0530

    Added solution - LeetHub

---
+++ Maximum Index - GFG/maximum-index.py
@@ -0,0 +1,26 @@
+#User function Template for python3
+class Solution:
+
+	def maxIndexDiff(self,arr,n):
+	    stack=[]
+	    for i in range(n):
+	        if(len(stack)==0 or arr[stack[-1]]>arr[i]):
+	            stack.append(i)
+	    ans=-1
+	    for i in range(n-1,-1,-1):
+	        while(len(stack)!=0 and arr[stack[-1]]<=arr[i]):
+	            ans=max(ans,i-stack.pop())
+	    return ans
+
+
+#{ 
+ # Driver Code Starts
+if __name__ == "__main__":
+	t = int(input())
+	while(t>0):
+		num = int(input())
+		arr = [int(x) for x in input().strip().split()]
+		ob = Solution()
+		print(ob.maxIndexDiff(arr,num))
+		t-=1
+# } Driver Code Ends

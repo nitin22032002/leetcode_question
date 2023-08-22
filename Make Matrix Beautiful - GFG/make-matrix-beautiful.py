709c69aeec3903cdd4ea6ceeda8b777d35b392e7
nitin22032002 <79556510+nitin22032002@users.noreply.github.com>
2023-08-22 08:22:15 +0530

    Added solution - LeetHub

---
+++ Make Matrix Beautiful - GFG/make-matrix-beautiful.py
@@ -0,0 +1,33 @@
+#User function Template for python3
+
+class Solution:
+    def findMinOpeartion(self, matrix, n):
+        row=[0 for _ in range(n)]
+        col=[0 for _ in range(n)]
+        for i in range(n):
+            for j in range(n):
+                row[i]+=matrix[i][j]
+                col[j]+=matrix[i][j]
+        target=max(row+col)
+        ans=0
+        for i in range(n):
+            for j in range(n):
+                r=max(row[i],col[j])
+                if(r<target):
+                    x=(target-r)
+                    ans+=x
+                    row[i]+=x
+                    col[j]+=x
+        return ans
+
+#{ 
+ # Driver Code Starts
+
+#Initial Template for Python 3
+
+for _ in range(int(input())):
+    n = int(input())
+    matrix = [list(map(int,input().split())) for _ in range(n)]
+    ob = Solution()
+    print(ob.findMinOpeartion(matrix, n))
+# } Driver Code Ends

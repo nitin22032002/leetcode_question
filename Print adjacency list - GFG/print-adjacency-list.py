f3f54074846b96776630191a93afa99d843a7c74
nitin22032002 <79556510+nitin22032002@users.noreply.github.com>
2023-09-05 08:26:52 +0530

    Added solution - LeetHub

---
+++ Print adjacency list - GFG/print-adjacency-list.py
@@ -0,0 +1,61 @@
+
+from typing import List
+
+
+class Solution:
+    def printGraph(self, V : int, edges : List[List[int]]) -> List[List[int]]:
+        graph=[[] for _ in range(V)]
+        for a,b in edges:
+            graph[a].append(b)
+            graph[b].append(a)
+        return graph
+        
+
+
+#{ 
+ # Driver Code Starts
+
+class IntArray:
+    def __init__(self) -> None:
+        pass
+    def Input(self):
+        arr=[int(i) for i in input().strip().split()]#array input
+        return arr
+    def Print(self,arr):
+        for i in arr:
+            print(i,end=" ")
+        print()
+
+
+
+class IntMatrix:
+    def __init__(self) -> None:
+        pass
+    def Input(self,n,m):
+        matrix=[]
+        #matrix input
+        for _ in range(n):
+            matrix.append([int(i) for i in input().strip().split()])
+        return matrix
+    def Print(self,arr):
+        for i in arr:
+            for j in i:
+                print(j,end=" ")
+            print()
+
+
+if __name__=="__main__":
+    t = int(input())
+    for _ in range(t):
+        
+        V,E=IntArray().Input()
+        
+        
+        edges=IntMatrix().Input(E, 2)
+        
+        obj = Solution()
+        res = obj.printGraph(V, edges)
+        
+        for row in res:
+            print(*sorted(row))
+# } Driver Code Ends

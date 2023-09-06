50b3c7ef9c9ea5ebcb72fa0f6f03be4f0f2264f2
nitin22032002 <79556510+nitin22032002@users.noreply.github.com>
2023-09-06 10:26:48 +0530

    Added solution - LeetHub

---
+++ Mother Vertex - GFG/mother-vertex.py
@@ -0,0 +1,43 @@
+
+class Solution:
+    
+    #Function to find a Mother Vertex in the Graph.
+	def findMotherVertex(self, V, adj):
+	    visited=[False]*V
+	    for i in range(V):
+	        if(visited[i]==False):
+	            self.get(adj,i,visited)
+	            vertex=i
+	    vis=[False]*V
+	    self.get(adj,vertex,vis)
+	    for item in vis:
+	        if(not item):return -1
+	    return vertex
+	def get(self,graph,node,visited):
+	    if(visited[node]):
+	        return
+	    else:
+	        visited[node]=True
+	        for item in graph[node]:
+	            self.get(graph,item,visited)
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import sys 
+
+sys.setrecursionlimit(10**6) 
+if __name__ == '__main__':
+	T=int(input())
+	for i in range(T):
+		V, E = map(int, input().split())
+		adj = [[] for i in range(V)]
+		for _ in range(E):
+			u, v = map(int, input().split())
+			adj[u].append(v)
+		obj = Solution()
+		ans = obj.findMotherVertex(V, adj)
+		print(ans)
+# } Driver Code Ends

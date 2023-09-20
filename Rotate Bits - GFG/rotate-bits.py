376c1cfcc25c936d18fa683d727bf3a90c210547
nitin22032002 <79556510+nitin22032002@users.noreply.github.com>
2023-09-20 07:54:32 +0530

    Added solution - LeetHub

---
+++ Rotate Bits - GFG/rotate-bits.py
@@ -0,0 +1,23 @@
+#User function Template for python3
+
+class Solution:
+    def rotate(self, N, D):
+        D%=16
+        left=(N>>D)|(N<<(16-D))&((1<<16)-1)
+        right=((N<<D)|(N>>(16-D)))&((1<<16)-1)
+        return [right,left]
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        n, d = input().strip().split(" ")
+        n, d = int(n), int(d)
+        ob = Solution()
+        ans = ob.rotate(n, d)
+        print(ans[0])
+        print(ans[1])
+# } Driver Code Ends

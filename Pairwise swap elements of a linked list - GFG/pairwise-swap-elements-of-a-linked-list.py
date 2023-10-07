64624f792b8fc0f7a0b78e4aece16d09f6a9dfc3
nitin22032002 <79556510+nitin22032002@users.noreply.github.com>
2023-10-07 07:49:03 +0530

    Added solution - LeetHub

---
+++ Pairwise swap elements of a linked list - GFG/pairwise-swap-elements-of-a-linked-list.py
@@ -0,0 +1,89 @@
+"""  list Node is as defined below:
+
+class Node:
+    def __init__(self, data):
+		self.data = data
+		self.next = None
+
+"""
+
+# complete this function
+# return head of list after swapping
+class Solution:    
+    def pairWiseSwap(self, head):
+        curr=head
+        prv=None
+        while(curr and curr.next):
+            if(prv==None):
+                head=curr.next
+            else:
+                prv.next=curr.next
+            t=curr.next.next
+            curr.next.next=curr
+            curr.next=t
+            prv=curr
+            curr=t
+        return head
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+    
+    def insert(self,val):
+        if self.head is None:
+            self.head = Node(val)
+            self.tail = self.head
+        else:
+            self.tail.next = Node(val)
+            self.tail = self.tail.next
+
+def printList(n):
+    while n:
+        print(n.data, end=' ')
+        n = n.next
+    print()
+
+if __name__=="__main__":
+    for _ in range(int(input())):
+        n = int(input())
+        arr = [int(x) for x in input().split()]
+        
+        lis = LinkedList()
+        for i in arr:
+            lis.insert(i)
+            
+        dict1 = {}
+        temp = lis.head
+        while temp:
+            dict1[temp] = temp.data
+            temp = temp.next
+        
+        failure = LinkedList()
+        failure.insert(-1)
+        
+        head = Solution().pairWiseSwap(lis.head)
+        
+        temp = head
+        f = 0
+        while temp:
+            if dict1[temp] != temp.data:
+                f = 1;
+            temp = temp.next
+        
+        if f:
+            printList(failure)
+        else:
+            printList(head)
+
+# } Driver Code Ends

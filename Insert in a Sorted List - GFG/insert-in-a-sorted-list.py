84a0302d956edfcbe6557bce335f8f8954c26837
nitin22032002 <79556510+nitin22032002@users.noreply.github.com>
2023-10-08 08:50:46 +0530

    Added solution - LeetHub

---
+++ Insert in a Sorted List - GFG/insert-in-a-sorted-list.py
@@ -0,0 +1,74 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# Node Class
+class Node:
+    def __init__(self, data):   # data -> value stored in node
+        self.data = data
+        self.next = None
+        
+# Linked List Class
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.prev=self.head
+    
+    # creates a new node with given value and appends it at the end of the linked list
+    def append(self, new_value):
+        new_node = Node(new_value)
+        if self.head is None:
+            self.head = new_node
+            self.prev = self.head
+        else:
+            self.prev.next = new_node
+            self.prev = self.prev.next
+
+def printList(head):
+    while head:
+        print(head.data, end=' ')
+        head = head.next
+    print()
+
+
+# } Driver Code Ends
+#User function Template for python3
+
+class Solution:
+    def sortedInsert(self, head1,key):
+        prv=None
+        t=head1
+        while(t):
+            if(t.data>key):
+                r=Node(key)
+                r.next=t
+                if(not prv):
+                    head1=r
+                else:
+                    prv.next=r
+                break
+            else:
+                prv=t
+            t=t.next
+        else:
+            r=Node(key)
+            prv.next=r
+        return head1
+
+
+#{ 
+ # Driver Code Starts.
+if __name__ == '__main__':
+    for _ in range(int(input())):
+        n = int(input())
+        
+        a = LinkedList()
+        nodes = list(map(int, input().strip().split()))
+        for x in nodes:
+            a.append(x)
+        
+        key=int(input())
+        h=Solution().sortedInsert(a.head,key)
+        printList(h)
+
+# } Driver Code Ends

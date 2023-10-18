a11bc44a4dbe0d94dd11f6f4aae05e9f76bd4973
nitin22032002 <79556510+nitin22032002@users.noreply.github.com>
2023-10-18 08:08:34 +0530

    Added solution - LeetHub

--- Eventual Safe States - GFG/eventual-safe-states.py
+++ Eventual Safe States - GFG/eventual-safe-states.py
@@ -4,7 +4,6 @@ from typing import List
 
 class Solution:    
     def eventualSafeNodes(self, V : int, adj : List[List[int]]) -> List[int]:
-        
         visited=[False]*V
         isSafe=[False]*V
         ans=[]
